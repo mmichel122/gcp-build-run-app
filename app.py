d2c4a2ebf7c67ad70389827086e524d211332d38
Mikael Michelier <mikael.michelier@googlemail.com>
2019-12-11 20:15:35 +0000

    New app.py version

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello():
-    target = os.environ.get('TARGET', 'Cloud Run')
+    target = os.environ.get('TARGET', 'GOOGLE CLOUD PLATFORM')
     return 'Greetings from {}!!!'.format(target)
 
 if __name__ == "__main__":
